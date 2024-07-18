b81f2ce26939e51efedc4be6cff515fa90c9323c
Faraz Fayyaz <113796246+farazfayyaz@users.noreply.github.com>
2024-07-18 13:17:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const btnContainer = document.getElementById('btn-container')
 
 const yesBtn = document.getElementById('yes-btn')
 const noBtn = document.getElementById('no-btn')
+const goBack = document.getElementById('go-back')
+
 
 mainScreen.classList.toggle('main-hidden')
 btnContainer.classList.toggle('btn-reverse')
@@ -16,4 +18,9 @@ yesBtn.addEventListener('click', () => {
 
 noBtn.addEventListener('click', () => {
     btnContainer.classList.toggle('btn-reverse')
+})
+
+goBack.addEventListener('click', () => {
+    mainScreen.classList.toggle('main-hidden')
+    yesMessage.classList.toggle('yes-hidden')
 })
